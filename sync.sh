a94d5f5bdd0dfa4cab3413e35b28a988b46f8385
ZhuXY <tonyzhu77@126.com>
2025-11-29 11:56:44 +0800

    Use --checksum and ssh in sync.ch

--- sync.sh
+++ sync.sh
@@ -10,4 +10,4 @@ cd "$LOCAL_PATH" || exit 1
 ./venv/bin/python3 -m scraper.step_3_download_attachments --board_id "$BOARD_ID" --mode $MODE || exit 1
 ./venv/bin/python3 -m scraper.step_4_render --board_id "$BOARD_ID" || exit 1
 
-rsync -az --stats --delete --exclude='venv/' --exclude='**/.DS_Store' ./ ali:~/bbs_scraper/
+rsync -az --rsh=ssh --stats --checksum --delete --exclude='venv/' --exclude='**/.DS_Store' ./ ali:~/bbs_scraper/
