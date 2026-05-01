b78aad153ac6fa4f26eb81653731e6a5c5b86b2e
ZhuXY <tonyzhu77@126.com>
2026-05-01 21:06:39 +0800

    [sync.sh] Move to clab server; No rsync

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOCAL_PATH="$HOME/Projects/bbs_scraper"
+LOCAL_PATH="$HOME/bbs_scraper"
 BOARD_ID="$1"
 MODE="update"
 
@@ -12,6 +12,6 @@ date
 ./venv/bin/python3 -m scraper.step_3_download_attachments --board_id "$BOARD_ID" --mode $MODE || exit 1
 ./venv/bin/python3 -m scraper.step_4_render --board_id "$BOARD_ID" || exit 1
 
-rsync -az --rsh=ssh --stats --checksum --delete --exclude='venv/' --exclude='**/.DS_Store' ./ ali:~/bbs_scraper/
+# rsync -az --rsh=ssh --stats --checksum --delete --exclude='venv/' --exclude='**/.DS_Store' ./ ali:~/bbs_scraper/
 
 date
